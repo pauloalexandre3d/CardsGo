0fc9d5c1b02ded94bb451b1ecd9ddbe2dddf41e7
Paulo Machado <pauloalexandre3d@gmail.com>
2019-04-10 17:55:40 +0200

    feature: Adding new methods, saveDeck, readDeck, shuffle, deal

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	card := newCard()
-	fmt.Println(card)
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards := newDeck()
+	cards.shuffle()
+	cards.print()
 }
